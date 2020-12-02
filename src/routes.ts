39f60c385df8552cde4efa2276d79448ec12ddd3
Silva404 <brenosilva0252@gmail.com>
2020-12-02 19:46:58 -0300

    feat: add users routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
+import CreateUserService from 'services/CreateUserService';
 
 const routes = Router();
 
 routes.use('/users');
 
-routes.post('/', (request, response) => {
+routes.post('/', async (request, response) => {
   const { name, email, password } = request.body;
+  const userService = new CreateUserService();
 
-  const user = await 
+  const user = await userService.execute({ name, email, password });
+  delete user.password;
 
-  return
+  return response.json(user);
 });
 
 export default routes;
