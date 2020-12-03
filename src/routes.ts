aff261c7bd733cbf3577aebcbae6da4d8c791251
Silva404 <brenosilva0252@gmail.com>
2020-12-02 23:36:40 -0300

    feat: add user login to routes

--- src/routes.ts
+++ src/routes.ts
@@ -8,7 +8,6 @@ routes.post('/users/register', async (request, response) => {
   const { name, email, password } = request.body;
 
   const userService = new CreateUserService();
-
   const user = await userService.execute({ name, email, password });
   delete user.password;
 
@@ -18,11 +17,18 @@ routes.post('/users/register', async (request, response) => {
 routes.post('/users/login', async (request, response) => {
   const { email, password } = request.body;
 
+  const authService = new AuthenticateUserService();
+  const { token } = await authService.execute({ email, password });
+
+  return response.json(token);
+});
+
+routes.get('/users/profile', async (request, response) => {
   const userService = new AuthenticateUserService();
 
-  const { user, token } = await userService.execute({ email, password });
+  const user = userService.execute();
 
-  return response.json({ user, token });
+  return response.json(user);
 });
 
 export default routes;
