c06eca3bc3060d1e6cfe3495aa8650cc31b48283
Silva404 <brenosilva0252@gmail.com>
2020-12-14 23:12:59 -0300

    feat: add CreatePosts migration

---
+++ src/database/migrations/1607971485963-CreatePosts.ts
@@ -0,0 +1,50 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreatePosts1607971485963 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'posts',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'content',
+            type: 'varchar',
+          },
+          {
+            name: 'user_id',
+            type: 'varchar',
+          },
+          {
+            name: 'votes',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('posts');
+  }
+}
