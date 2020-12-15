f53e8d2612f1ff7b7fad9b86261e546c776d2621
Silva404 <brenosilva0252@gmail.com>
2020-12-14 23:12:37 -0300

    feat: add cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(8080, () => {
   console.log('✨ Server is running');
 });
