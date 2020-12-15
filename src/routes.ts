115c47b331e12a01cc614bd79b1c76130ff35798
Silva404 <brenosilva0252@gmail.com>
2020-12-14 23:22:15 -0300

    feat: add users posts routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+
+import ensureAuthenticated from './middlewares/ensureAuthenticated';
+
+import UpvotePostService from './services/UpvotePostService';
+import ListUserPosts from './services/ListUserPosts';
+import CreatePostService from './services/CreatePostService';
 import AuthenticateUserService from './services/AuthenticateUserService';
 import CreateUserService from './services/CreateUserService';
 
 const routes = Router();
+// routes.use(ensureAuthenticated);
 
 routes.post('/users/register', async (request, response) => {
   const { name, email, password } = request.body;
@@ -23,12 +30,48 @@ routes.post('/users/login', async (request, response) => {
   return response.json(token);
 });
 
-routes.get('/users/profile', async (request, response) => {
-  const userService = new AuthenticateUserService();
+routes.post(
+  '/users/create-post',
+  ensureAuthenticated,
+  async (request, response) => {
+    const { id, title, content, votes } = request.body;
 
-  const user = userService.execute();
+    const createPostService = new CreatePostService();
 
-  return response.json(user);
+    const post = await createPostService.execute({ id, title, content, votes });
+
+    return response.json(post);
+  },
+);
+
+routes.patch(
+  '/users/upvote-post',
+  ensureAuthenticated,
+  async (request, response) => {
+    const { id, votes } = request.body;
+
+    const upvotePostService = new UpvotePostService();
+
+    const post = await upvotePostService.execute({ id, votes });
+
+    return response.json(post);
+  },
+);
+
+routes.get('/users/posts', ensureAuthenticated, async (request, response) => {
+  const listUserPostsService = new ListUserPosts();
+
+  const posts = await listUserPostsService.execute({ id: request.user.id });
+
+  return response.json(posts);
 });
+
+// routes.get('/users/profile', async (request, response) => {
+//   const userService = new AuthenticateUserService();
+
+//   const user = userService.execute();
+
+//   return response.json(user);
+// });
 
 export default routes;
