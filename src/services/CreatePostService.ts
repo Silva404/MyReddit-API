81239631010d61099581b163d3618022201b2597
Silva404 <brenosilva0252@gmail.com>
2020-12-15 22:38:39 -0300

    feat: add CreatePostService

--- src/services/CreatePostService.ts
+++ src/services/CreatePostService.ts
@@ -1,29 +1,31 @@
-import Post from 'models/Post';
 import { getCustomRepository } from 'typeorm';
+
+import Post from 'models/Post';
 import PostRepository from '../repositories/PostRepository';
-import UserRepository from '../repositories/UserRepository';
 
 interface IRequest {
-  id: string;
+  subreddit_id: string;
+  user_id: string;
   title: string;
   content: string;
   votes: number;
 }
 
 export default class CreatePostService {
-  public async execute({ id, title, content, votes }: IRequest): Promise<Post> {
-    const userRepository = getCustomRepository(UserRepository);
+  public async execute({
+    subreddit_id,
+    user_id,
+    title,
+    content,
+    votes,
+  }: IRequest): Promise<Post> {
     const postRepository = getCustomRepository(PostRepository);
-    const user = await userRepository.findOne({ where: { id } });
-
-    if (!user) {
-      throw new Error('User is not registered.');
-    }
 
     const post = postRepository.create({
       title,
       content,
-      user_id: id,
+      subreddit_id,
+      user_id,
       votes,
     });
 
