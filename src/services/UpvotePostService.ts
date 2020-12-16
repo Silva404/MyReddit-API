d7660681c51c9e32414b8e1fc96347a59fb557c0
Silva404 <brenosilva0252@gmail.com>
2020-12-15 22:00:37 -0300

    feat: add UpvotePostService

--- src/services/UpvotePostService.ts
+++ src/services/UpvotePostService.ts
@@ -1,27 +1,18 @@
 import { getCustomRepository } from 'typeorm';
 import Post from '../models/Post';
 import PostRepository from '../repositories/PostRepository';
-import UserRepository from '../repositories/UserRepository';
 
 interface IRequest {
-  id: string;
   votes: number;
+  id: string;
+  user_id: string;
 }
 
 export default class UpvotePostService {
-  public async execute({ id, votes }: IRequest): Promise<Post> {
-    const userRepository = getCustomRepository(UserRepository);
+  public async execute({ votes, user_id, id }: IRequest): Promise<Post> {
     const postRepository = getCustomRepository(PostRepository);
-    const user = await userRepository.findOne({ where: { id } });
-
-    if (!user) {
-      throw new Error('User is not registered.');
-    }
-
-    const post = postRepository.create({
-      user_id: id,
-      votes,
-    });
+    const post = await postRepository.findOne({ where: { user_id, id } });
+    post.votes = votes;
 
     await postRepository.save(post);
 
