e76f596333f5258a859c3b7f4873752617db3eab
Silva404 <brenosilva0252@gmail.com>
2020-12-15 21:57:44 -0300

    feat: add CreateSubredditPost  migration

---
+++ src/database/migrations/1608078056207-CreatePostsComments.ts
@@ -0,0 +1,47 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreatePostsComments1608078056207
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'comments',
+        columns: [
+          {
+            name: 'post_id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'user_id',
+            type: 'varchar',
+          },
+          {
+            name: 'comment',
+            type: 'varchar',
+          },
+          {
+            name: 'votes',
+            type: 'int',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('comments');
+  }
+}
